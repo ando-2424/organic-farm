c567491b3691ca444cb01b9af1f71f1a25a1cf4a
ando-2424 <yu.a.0713.24@gmail.com>
2025-02-27 00:11:11 +0900

    02/27 PC用のheader完成

--- resources/views/pages/layouts/header.blade.php
+++ resources/views/pages/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <header class="header inner">
   <div class="header-section">
     <div class="header-section__logo-area">
-      <h1>
+      <h1 class="header-section__logo-item">
         <a href="{{ route('top.index') }}">
           <img src="{{ asset('images/header/organicfarm_log.png')}}" alt="organicfarm">
         </a>
@@ -9,15 +9,14 @@
     </div>
     <div class="header-section__menu">
       <nav class="header-section__company-menu__area">
-        <ul class="header-section__menu__list">
+        <ul class="header-section__company-menu__list">
           <li class="header-section__company-menu__item"><a href="{{ route('top.index') }}">Home</a></li>
           <li class="header-section__company-menu__item"><a href="{{ route('top.index') }}">About</a></li>
-          <li class="header-section__company-menu__item"><a href="{{ route('top.index') }}">Access</a></li>
           <li class="header-section__company-menu__item"><a href="{{ route('top.index') }}">Contact</a></li>
         </ul>
       </nav>
       <nav class="header-section__service-menu__area">
-        <ul class="header-section__menu__list">
+        <ul class="header-section__service-menu__list">
           <li class="header-section__service-menu__item"><a href="{{ route('top.index') }}">Farm</a></li>
           <li class="header-section__service-menu__item"><a href="{{ route('top.index') }}">Vegtable</a></li>
           <li class="header-section__service-menu__item"><a href="{{ route('top.index') }}">Workshop</a></li>
