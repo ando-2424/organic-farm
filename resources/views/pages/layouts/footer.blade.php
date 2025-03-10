bd9cbdca8cf74a205df05fe9f33a7a6c06d83d2e
ando-2424 <yu.a.0713.24@gmail.com>
2025-03-10 23:16:19 +0900

    0310 footerのlogo・snslogoとレイアウト調整途中

---
+++ resources/views/pages/layouts/footer.blade.php
@@ -0,0 +1,26 @@
+<footer class="footer">
+  <div class="inner">
+    <div class="footer-image">
+      <img src="{{ asset('images/footer/icon.png')}}" alt="organicfarm">
+    </div>
+    <div class="">
+      <div>
+        <ul class="footer__sns-list">
+          <li class="footer__sns-list__item"><a href="#"><img src="{{ asset('images/footer/facebook_logo.png')}}" alt="facebookのロゴ"></a></li>
+          <li class="footer__sns-list__item"><a href="#"><img src="{{ asset('images/footer/Instagram_logo.png')}}" alt="Instagramのロゴ"></a></li>
+          <li class="footer__sns-list__item"><a href="#"><img src="{{ asset('images/footer/x_logo.png')}}" alt="xのロゴ"></a></li>
+        </ul>
+      </div>
+      <div>
+        <ul class="footer__menu-list">
+          <li class="footer__menu-list__item"><a href="./">Home</a></li>
+          <li class="footer__menu-list__item"><a href="#about">About</a></li>
+          <li class="footer__menu-list__item"><a href="#contact">Contact</a></li>
+          <li class="footer__menu-list__item"><a href="#">Farm</a></li>
+          <li class="footer__menu-list__item"><a href="vegtable/">Vegtable</a></li>
+          <li class="footer__menu-list__item"><a href="works/">Workshop</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
